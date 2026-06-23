2c84208b166650ae00125bcd7adea23c2ca0ce4e
thiagosullivan <thiagosullivan12@gmail.com>
2026-06-22 23:43:27 -0300

    feat: signin page

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+export default function SignInPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div>
+        <Link href="/">Go Back Home</Link>
+      </div>
+    </div>
+  );
+}
