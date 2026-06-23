9dea12866975d80890042506a69bb94bd7de2b77
thiagosullivan <thiagosullivan12@gmail.com>
2026-06-23 19:21:07 -0300

    feat: SignIn component on Signin page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,10 +1,12 @@
+import { SignIn } from "@hexclave/next";
 import Link from "next/link";
 import React from "react";
 
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div>
+      <div className="max-w-md w-full space-y-8">
+        <SignIn />
         <Link href="/">Go Back Home</Link>
       </div>
     </div>
