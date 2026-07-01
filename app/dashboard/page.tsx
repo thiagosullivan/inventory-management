645880ab04167be554a4a8a5872dae409fdb1d64
thiagosullivan <thiagosullivan12@gmail.com>
2026-06-30 22:42:14 -0300

    feat: create dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,28 @@
+import SideBar from "@/components/sidebar";
+import React from "react";
+
+export default async function DashboardPage() {
+  // const totalProducts = await
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <SideBar currentPath="/dashboard" />
+      <main className="ml-64 p-8">
+        <div className="mb-8">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-semibold text-gray-900">
+                Dashboard
+              </h1>
+              <p className="text-sm text-gray-500">
+                Welcome back! Here is an overview of your inventory.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Key Metrics */}
+      </main>
+    </div>
+  );
+}
